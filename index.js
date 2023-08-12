dcb05af9d2044cd55f8c35d3b451ac488141c477
DoubledConG <49092777+cccoding365@users.noreply.github.com>
2023-08-12 11:09:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const setScale = () => {
-    let clientWidth = document.documentElement.clientWidth
-    let clientHeight = document.documentElement.clientHeight
-    let designWidth = 1920
-    let designHeight = 1080
-    let scale = Math.min((clientWidth / designWidth), (clientHeight / designHeight))
-    document.querySelector('#container').style.transform = `scale(${scale}) translate(-50%,-50%)`
-}
-window.onload = setScale
-window.onresize = setScale
+            let clientWidth = document.documentElement.clientWidth
+            let clientHeight = document.documentElement.clientHeight
+            let designWidth = document.getElementById('container').offsetWidth
+            let designHeight = document.getElementById('container').offsetHeight
+            let scale = Math.min((clientWidth / designWidth), (clientHeight / designHeight))
+            document.querySelector('#container').style.transform = `scale(${scale}) translate(-50%,-50%)`
+        }
+        window.addEventListener('load', setScale)
+        window.addEventListener('resize', setScale)
